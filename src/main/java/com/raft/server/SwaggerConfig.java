70293c230a7fa51339e77739b668d21c4835bdb4
xopose <lazarevds2001@gmail.com>
2024-12-22 16:55:55 +0300

    add database

--- src/main/java/com/raft/server/SwaggerConfig.java
+++ src/main/java/com/raft/server/SwaggerConfig.java
@@ -25,7 +25,6 @@ public class SwaggerConfig {
                 .licenseUrl(null)
                 .termsOfServiceUrl("")
                 .version("1.0.0")
-                .contact(new Contact("", "", "pleshakoff@gmail.com"))
                 .build();
     }
 
